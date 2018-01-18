6666f89ae125be3720a70b63dca1d168a0fcd799
Vinicius Souza <hi@vsouza.com>
2018-01-18 19:14:48 -0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GraphQLHandler",
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3, minor: 0),
         .Package(url: "https://github.com/GraphQLSwift/GraphQL.git", majorVersion: 0, minor: 3)
     ]
 )
